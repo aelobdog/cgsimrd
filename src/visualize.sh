0188f9f1cb61becf7c735318a45ba91ca60ef745
Ashwin Godbole <dev.godboleashwin@gmail.com>
2021-08-30 19:27:46 +0530

    moved from .xyz to .dat; from vmd to oxview

--- src/visualize.sh
+++ src/visualize.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
-# usage : sh simulate.sh <pdbname>
+# usage : sh simulate.sh pdbname temperature molarity
 usage() {
-   echo "usage : sh visualize.sh <pdbname> <temperature> <molarity> [vis software]"
+   echo "usage : sh visualize.sh pdbname temperature molarity" 
 }
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 3 ]; then
    echo "\e[0;31mERROR\e[0m: Check number of arguments."
-   echo "  └── Expected 4, got $#\n"
+   echo "  └── Expected 3, got $#\n"
    usage
    exit 1
 fi
 
 temp=`echo $2 | sed "s/\./_/g"`
 conc=`echo $3 | sed "s/\./_/g"`
-input=$1-${temp}-${conc}.xyz
-echo $input
+input=$1-${temp}-${conc}
 
-if [ ! -f sim/$input ]; then
-   echo "\e[0;31mERROR\e[0m: trajectory file does NOT EXIST for "
+if [ ! -f sim/$input.dat ]; then
+   echo "\e[0;31mERROR\e[0m: trajectory file does NOT EXIST for"
    echo " |---  molecule      : $1"
    echo " |---  temperature   : $2"
    echo " |---  concentration : $3"
    exit 1
 fi
 
-if ! $4 sim/$input; then
-   echo "\e[0;31mERROR\e[0m: could not visualize the file using $4"
-   echo "\e[0;31mERROR\e[0m: check your installation for $4"
-fi
+cd oxdna_files/
+python3 ../lib/tacoxDNA/src/LAMMPS_oxDNA.py ../lmp/$1.data ../sim/${input}.dat
+mv $1.data.oxdna ${input}.oxdna
+mv $1.data.top ${input}.top
+oxview ${input}.oxdna ${input}.top
+cd ..
+
